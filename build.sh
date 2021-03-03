09d4bb672f3860b7c67ca9ac2d63b6dc23ef9ae6
Marius <mariusdieckmann90@gmail.com>
2021-03-03 19:54:02 +0100

    initial commit

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+rm -rf API
+git clone git@github.com:ag-computational-bio/bakta-web-api.git
+
+mkdir build
+
+# Builds the go stubs from the protobuf files and places them in their directories
+# Temporary solution until ci/cd is enabled 
+cd bakta-web-api
+git checkout main
+protoc --proto_path=. --go-grpc_out=../build --go_out=../build --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative proto/api/*.proto
+protoc -I . --grpc-gateway_out ./proto/build --grpc-gateway_opt logtostderr=true --grpc-gateway_opt paths=source_relative proto/api/*
+
+protoc -I . --openapiv2_out ../build --openapiv2_opt logtostderr=true proto/api/*
+
+cd ..
+
+mv build/proto/api/Jobs.swagger.json swagger
+
+go generate swagger/SwaggerGen.go
+mv build/proto/api/*.go api
+
+rm -rf bakta-web-api
+rm -rf build
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/ag-computational-bio/bakta-web-api-go
+
+go 1.16
+
+require (
+	github.com/golang/protobuf v1.4.3
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0
+	google.golang.org/genproto v0.0.0-20210303154014-9728d6b83eeb
+	google.golang.org/grpc v1.36.0
+	google.golang.org/protobuf v1.25.0
+)
